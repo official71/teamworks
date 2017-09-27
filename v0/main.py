903df2e0508e7f576d9bcc4cfeb3d3514682fe76
Yi Qi <yqi71@hotmail.com>
2017-09-27 02:20:23 -0400

    colorful

--- v0/main.py
+++ v0/main.py
@@ -3,6 +3,49 @@
 import argparse
 from gsearch import *
 from rocchio import *
+from termcolor import colored
+from nltk.stem.snowball import SnowballStemmer
+
+nocol = False # no colors
+stemmer = SnowballStemmer("english")
+
+## color the string based on its type
+#  @param s string, type: str
+#  @param t type of string, type: str
+def color(s, t='default'):
+    if nocol or not s:
+        return s
+    # colorful
+    if t == 'rel':
+        return colored(s, 'green')
+    elif t == 'irrel':
+        return colored(s, 'red')
+    elif t == 'key':
+        return colored(s, 'green', attrs=['underline'])
+    elif t == 'delim':
+        return colored(s, 'yellow', attrs=['bold'])
+    elif t == 'strong':
+        return colored(s, 'magenta', attrs=['bold'])
+    elif t == 'error':
+        return colored(s, 'red')
+    else:
+        return colored(s, 'white')
+
+## highlight keywords in string
+#  @param string type: str
+#  @param keywords type: list[str]
+def highlight_keywords(string, keywords):
+    if nocol:
+        return string
+    string = string.encode('ascii', 'ignore')
+    keys = [stemmer.stem(w) for w in keywords]
+    res = []
+    for i, w in enumerate(string.split()):
+        if stemmer.stem(filter(str.isalnum, w.lower())) in keys:
+            res.append(color(w, 'key'))
+        else:
+            res.append(color(w))
+    return " ".join(res)
 
 ## validate input arguments
 def validate(args):
@@ -13,7 +56,7 @@ def validate(args):
             from secrets import GSEARCH_JSON_API
             api = GSEARCH_JSON_API
         except:
-            print "[ERROR] Search API not specified"
+            print color("[ERROR] Search API not specified", "error")
             exit(1)
     # validate search engine
     engine = args.engine
@@ -22,17 +65,18 @@ def validate(args):
             from secrets import GSEARCH_ENGINE
             engine = GSEARCH_ENGINE
         except:
-            print "[ERROR] Search engine ID not specified"
+            print color("[ERROR] Search engine ID not specified", "error")
             exit(1)
     # validate target precision
     target_precision = args.target_precision
     if not 0 < target_precision <= 1.0:
-        print "[ERROR] target precision {} not in range (0, 1]".format(target_precision)
+        print color("[ERROR] target precision {} not in range (0, 1]".format(
+            target_precision), "error")
         exit(1)
     # validate query
     query_terms = [unicode(i) for i in args.query.strip().split(' ')]
     if not query_terms:
-        print "[ERROR] empty/invalid query string"
+        print color("[ERROR] empty/invalid query string", "error")
         exit(1)
 
     # all set, return validated arguments
@@ -42,7 +86,7 @@ def validate(args):
 ## collect user feedbacks on the documents returned by search engine,
 #  the feedback is binary Y/N indicating whether the document is relevant.
 #  @param docs documents returned by search engine, type: list[SearchDocument]
-def feedback(docs):
+def feedback(docs, keywords):
     # to save human effort, we save the previous feedbacks in two files: 
     # "tmp/rel.txt" and "tmp/irrel.txt", where each row is the key of a doc
     def f2set(f):
@@ -65,28 +109,29 @@ def feedback(docs):
     c = 0
     for doc in docs:
         c += 1
-        print "\n" + "-" * 30 + format(c, '^3d') + "-" * 30
-        print "    URL: " + doc.url
-        print "  Title: " + doc.title
-        print "Snippet: " + doc.snippet
+        print color("-" * 30 + format(c, '^3d') + "-" * 30, "delim")
+        print color("    URL: " + doc.url)
+        print color("  Title: ") + highlight_keywords(doc.title, keywords)
+        print color("Snippet: ") + highlight_keywords(doc.snippet, keywords)
 
         if doc.key in rels:
-            print "\nalready marked relevant, continue..."
+            print "\n" + color("already marked relevant, continue...", "rel")
             rel.add(doc)
         elif doc.key in irrels:
-            print "\nalready marked irrelevant, continue..."
+            print "\n" + color("already marked irrelevant, continue...", "irrel")
             irrel.add(doc)
         else:
-            i = raw_input("\nIs this relevant (Y/N)? ")
+            i = raw_input("\n" + color("Is this relevant (" + \
+                color("Y", "rel") + color("/") + color("N", "irrel") + color(")? ")))
             if i.strip().lower() in ['y', 'yes']:
-                print "mark relevant..."
+                print color("marked relevant...", "rel")
                 rel.add(doc)
                 if rf: rf.write(doc.key + '\n')
             else:
-                print "mark irrelevant..."
+                print color("marked irrelevant...", "irrel")
                 irrel.add(doc)
                 if irf: irf.write(doc.key + '\n')
-    print "... Done\n"
+    print ""
 
     l1, l2 = len(rel), len(irrel)
     p = float(l1) / (l1 + l2) if l1 > 0 else 0
@@ -96,27 +141,34 @@ def feedback(docs):
 ## main
 def main(args):
     target_precision, query_terms, api, engine = validate(args)
-    iteration, precision = 0, 0.0
+    global nocol
+    nocol = args.nocol
+
+    # set Rocchio weights: relevant 0.75, irrelevant 0.25
     ro = Rocchio(0.75, 0.25)
+    iteration, precision = 0, 0.0
     while iteration == 0 or precision < target_precision:
-        print "=" * 80
-        print "[iteration:%2d]" % iteration,
-        print "query: " + " ".join(query_terms)
+        print color("=" * 80, "delim")
+        print color("[iteration: ", "bold") + color(str(iteration), "strong") + \
+              color("]", "bold")
+        print color("query: ", "bold") + color(" ".join(query_terms), "strong")
         # apply search on the current query terms
         docs = gsearch(" ".join(query_terms), api, engine)
         
         # collect user feedback
-        rel, irrel, precision = feedback(docs)
-        print "[iteration:%2d] %d relevant, %d irrelevant, %.1f precision" % \
-            (iteration, len(rel), len(irrel), precision)
+        rel, irrel, precision = feedback(docs, query_terms)
+        print color("[iteration: ", "bold") + color(str(iteration), "strong") + color("] ", "bold") + \
+              color(str(len(rel)), "strong") + color(" relevant, ", "bold") + \
+              color(str(len(irrel)), "strong") + color(" irrelevant, ", "bold") + \
+              color("%0.1f" % precision, "strong") + color(" precision", "bold")
 
         if precision >= target_precision:
             # target achieved
-            print "target precision %.1f has been achieved" % target_precision
+            print color("target precision %.1f has been achieved" % target_precision, "rel")
             break
         elif precision == 0:
             # no need to move on
-            print "no need to continue under zero precision"
+            print color("no need to continue under zero precision", "error")
             break
 
         # update query string
@@ -125,10 +177,10 @@ def main(args):
         iteration += 1
         if iteration >= 10:
             # maximum iteration
-            print "maximum iteration exceeded"
+            print color("maximum iteration exceeded", "error")
             break
 
-    print "Exit...\n"
+    print color("Exit...\n", "bold")
 
 
 
@@ -138,6 +190,7 @@ if __name__ == '__main__':
     parser.add_argument('query', type=str, help='initial query string')
     parser.add_argument('--api', type=str, help='Google search API key')
     parser.add_argument('--engine', type=str, help='Google search engine ID')
+    parser.add_argument('--nocol', action="store_true", help='Disable color prints')
 
     # args = vars()
     main(parser.parse_args())
